ca0d664a3abf6170bc45e2bb05caf2909cd40ab6
Rafael-Marian Vanca <109948174+RafaelMarian@users.noreply.github.com>
2025-10-06 21:31:55 +0300

    Updated API endpoint for Render

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   const handlePredict = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://ai-insights-2jf2.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ Age: parseInt(age), Income: parseInt(income) })
