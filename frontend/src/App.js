e6d7021621454559bd260f4d37ce2b703ceeef49
Rafael-Marian Vanca <109948174+RafaelMarian@users.noreply.github.com>
2025-10-07 09:28:47 +0300

    Connected React frontend to Render backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   const handlePredict = async () => {
     try {
-      const response = await fetch("${process.env.REACT_APP_API_URL}/predict", {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/predict`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ Age: parseInt(age), Income: parseInt(income) })
